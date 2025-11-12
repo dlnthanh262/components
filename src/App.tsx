4162181d2037713b970444a87a7d9ad717c6f5db
Thanh Do <dolenguyetthanh@gmail.com>
2025-11-12 22:25:45 +0700

    Create redeploy and deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, BlogReviewCardPage } from "@/pages";
 import "./App.css";
 
-const basename = import.meta.env.VITE_ROUTER_BASENAME || "/";
-
 function App() {
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route
